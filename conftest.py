858c89bd89bbe90d4db37328c1f8a057d7ee98a7
Stanlys <stanlys@yandex.ru>
2019-07-14 14:55:22 +0300

    Добавил открытие сайтов с использованием языка

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default=None,
@@ -10,10 +12,14 @@ def browser(request):
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         print("Browser {} still is not implemented".format(browser_name))
     yield browser
