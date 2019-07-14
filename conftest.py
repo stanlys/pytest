724ef9d185ffab1d1914d7faa395a0442e07b709
Stanlys <stanlys@yandex.ru>
2019-07-14 16:21:22 +0300

    It is work! WOW

--- conftest.py
+++ conftest.py
@@ -3,13 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
+
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox")
+                    help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="uk", help="Choose language for site")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         options = Options()
--- test_items.py
+++ test_items.py
@@ -1,6 +1,14 @@
+from selenium.common.exceptions import NoSuchElementException
+import time
+
 link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 
 def test_button_basket(browser):
     browser.get(link)
-    button = browser.find_element_by_css_selector(("[class='.btn-add-to-basket']")).text
-    assert button == "Добавить в корзину","My Error 1 "
+    time.sleep(30)
+    try:
+        button = browser.find_element_by_css_selector('.btn-add-to-basket')
+    except NoSuchElementException:
+        assert button == "Добавить в    корзину","My Error 1"
+    buttontext = button.text
+    print("--------{}----------".format(buttontext))
