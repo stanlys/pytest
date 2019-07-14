006f1488b2c217e69ab8213aeb729fea8600c532
Stanlys <stanlys@yandex.ru>
2019-07-14 15:37:19 +0300

    1 запуск

--- test_items.py
+++ test_items.py
@@ -2,4 +2,5 @@ link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 
 def test_button_basket(browser):
     browser.get(link)
-    button = browser.find_element_by_css_selector(("[class='..btn-add-to-basket']"))
+    button = browser.find_element_by_css_selector(("[class='.btn-add-to-basket']")).text
+    assert button == "Добавить в корзину","My Error 1 "
